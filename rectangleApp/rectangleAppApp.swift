8944d2b121d9a98fc875aa9d26b0df961ff5f5b4
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-04 10:59:50 +0900

    Initial Commit

---
+++ rectangleApp/rectangleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  rectangleAppApp.swift
+//  rectangleApp
+//
+//  Created by 神山駿 on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct rectangleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
